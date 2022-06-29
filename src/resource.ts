398b9bc2687abc6a6f09a5ad2f255550501526d3
tenpaMk2 <wavyroom@gmail.com>
2022-06-29 22:39:38 +0900

    :fire: remove unused mapchip code.

--- src/resource.ts
+++ src/resource.ts
@@ -1,7 +1,6 @@
 import { TiledMapResource } from "@excaliburjs/plugin-tiled";
 import { ImageSource, Sound } from "excalibur";
 
-const mapchipPng = "./roguelikeSheet_transparent.png";
 const tiledMapTmx = "./sample-stage.tmx";
 const dino = "./DinoSprites - vita.png";
 const dinoJump = "./open_002.ogg";
@@ -9,7 +8,6 @@ const dinoLanding = "./select_006.ogg";
 const dinoBlocked = "./error_004.ogg";
 
 export const Resources = {
-  mapchip: new ImageSource(mapchipPng),
   tiledmap: new TiledMapResource(tiledMapTmx),
   dino: new ImageSource(dino),
   dinoJump: new Sound(dinoJump),
