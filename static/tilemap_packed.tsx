d0b33b9db52609aee30c3765fe4c213794476cda
tenpaMk2 <wavyroom@gmail.com>
2022-06-29 20:19:29 +0900

    :bento: expand map.

---
+++ static/tilemap_packed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="tilemap_packed" tilewidth="24" tileheight="24" tilecount="12" columns="6">
+ <image source="tilemap_packed.png" width="144" height="48"/>
+</tileset>
